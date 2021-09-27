4089cd940c7c11ad6c947df37d695ba8f6b3a4db
Luiz Eduardo <eduardoveltroni@hotmail.com>
2021-09-27 09:11:40 -0300

    feat: adiciona animate nas div conforme o scroll

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+
+// Animação das div
+const target = document.querySelectorAll('[data-animation]')
+const animationClass = 'animate'
+
+function animationScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+
+  target.forEach(function(element) {
+      if (windowTop > element.offsetTop ) {
+          element.classList.add(animationClass)
+      } else {
+          element.classList.remove(animationClass)
+      }
+  })
+}
+
+window.addEventListener('scroll', () => animationScroll())
